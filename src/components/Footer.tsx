2398558b8827a54d57d6f3947628d9273706d82e
Pragya Gaur <gaur273pragya@gmail.com>
2024-07-05 13:51:22 +0530

    fixed footer section

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,19 +7,22 @@ import {
   faInstagram,
   faLinkedin,
 } from "@fortawesome/free-brands-svg-icons";
+import RevolverMapsWidget from "./RevolverMapsWidget";
 
 const Footer = () => {
   return (
     <Box sx={{ bgcolor: "primary.main", color: "white", p: 4 }}>
       <Container maxWidth="lg">
         <Grid container spacing={4}>
-          <Grid item xs={12} sm={4}>
+          <Grid item xs={12} sm={3}>
             <Typography variant="h6" gutterBottom>
               Aftab Women Welfare Society
             </Typography>
-            <Typography variant="body1">About</Typography>
+            <Typography variant="body1">
+              Empowering women through legal support and awareness.
+            </Typography>
           </Grid>
-          <Grid item xs={12} sm={4}>
+          <Grid item xs={12} sm={3}>
             <Typography variant="h6" gutterBottom>
               Mail Us
               <IconButton color="inherit" aria-label="mail us">
@@ -32,29 +35,19 @@ const Footer = () => {
                 <Phone />
               </IconButton>
             </Typography>
-            <Typography variant="h6" gutterBottom>
-              Find Us
-              <IconButton color="inherit" aria-label="find us">
-                <LocationOn />
-              </IconButton>
-            </Typography>
           </Grid>
-          <Grid item xs={12} sm={4}>
+          <Grid item xs={12} sm={3}>
             <Typography variant="h6" gutterBottom>
               Follow Us
             </Typography>
             <IconButton color="inherit" aria-label="Facebook">
               <FontAwesomeIcon icon={faFacebook} />
             </IconButton>
-            <IconButton color="inherit" aria-label="Twitter">
-              <FontAwesomeIcon icon={faTwitter} />
-            </IconButton>
-            <IconButton color="inherit" aria-label="Instagram">
-              <FontAwesomeIcon icon={faInstagram} />
-            </IconButton>
-            <IconButton color="inherit" aria-label="LinkedIn">
-              <FontAwesomeIcon icon={faLinkedin} />
-            </IconButton>
+          </Grid>
+          <Grid item xs={12} sm={3}>
+            <Typography variant="h6" gutterBottom>
+              Find Us
+            </Typography>
           </Grid>
         </Grid>
         <Box textAlign="center" mt={4}>
@@ -62,13 +55,13 @@ const Footer = () => {
             Aftab Women Welfare © 2024
           </Typography>
           <Box display="flex" justifyContent="center" alignItems="center">
-            <Typography variant="body2">
+            <Typography variant="body2" mr={1}>
               Website by - Pragya Gaur
             </Typography>
             <IconButton
               color="inherit"
               aria-label="LinkedIn"
-              href="https://www.linkedin.com/"
+              href="www.linkedin.com/in/pragyagaur12"
             >
               <FontAwesomeIcon icon={faLinkedin} />
             </IconButton>
